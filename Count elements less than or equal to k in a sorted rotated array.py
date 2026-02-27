3199646ad8a65ade264254bbdb5fe7153eee780a
asheeshkgithb <asheesh.kumar.cs28@iilm.edu>
2026-02-27 11:31:57 +0530

    Create Count elements less than or equal to k in a sorted rotated array.py

---
+++ Count elements less than or equal to k in a sorted rotated array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countLessEqual(self, arr, x):
+        count = 0
+        for num in arr:
+            if num <= x:
+                count += 1
+        return count
+        
